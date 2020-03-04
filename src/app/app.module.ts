5088ba769ff0bd3e2b16f6c3b8fa76fdb36e9629
thiaskjaer <s.thias@gmail.com>
2020-03-04 11:45:18 +0000

    added firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule,
     NoopAnimationsModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAy4A85p6zWO7Am1HnyNIjWi07NZ__KTMo",
+    authDomain: "ble-presence-detector.firebaseapp.com",
+    databaseURL: "https://ble-presence-detector.firebaseio.com",
+    projectId: "ble-presence-detector",
+    storageBucket: "ble-presence-detector.appspot.com",
+    messagingSenderId: "346242733028",
+    appId: "1:346242733028:web:71b35e1c535a573f876df1"
+  }
 };
 
 /*

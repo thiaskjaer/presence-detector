74f08377710780d26dcda339f502353ef22664f8
Thias Kjaer <s.thias@gmail.com>
2020-03-04 15:40:00 +0000

    add firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAy4A85p6zWO7Am1HnyNIjWi07NZ__KTMo",
+    authDomain: "ble-presence-detector.firebaseapp.com",
+    databaseURL: "https://ble-presence-detector.firebaseio.com",
+    projectId: "ble-presence-detector",
+    storageBucket: "ble-presence-detector.appspot.com",
+    messagingSenderId: "346242733028",
+    appId: "1:346242733028:web:71b35e1c535a573f876df1"
+  }
 };
